1545f87333d2557e82c3d1f715689fb9d918d4dc
Harsha Lakamsani <hlakamsani@gmail.com>
2023-05-15 02:48:40 -0700

    base commit

--- next.config.js
+++ next.config.js
@@ -1,2 +1,17 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+module.exports = withNextra({
+    images: {
+        remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'i.ibb.co',
+            port: '',
+        },
+        {
+            protocol: 'https',
+            hostname: 'gravatar.com',
+            port: '',
+        },
+        ],
+    },
+})
--- theme.config.js
+++ theme.config.js
@@ -2,8 +2,9 @@ const YEAR = new Date().getFullYear();
 
 export default {
   footer: (
-    <small style={{ display: "block", marginTop: "8rem" }}>
-      <time>{YEAR}</time> © Create Nextra App.
+    <small style={{ display: "block", marginTop: "4rem" }}>
+      <abbr title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.">
+        CC BY-NC 4.0</abbr> <time>{YEAR}</time> © Harsha Lakamsani.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
@@ -12,7 +13,7 @@ export default {
         @media screen and (max-width: 480px) {
           article {
             padding-top: 2rem;
-            padding-bottom: 4rem;
+            padding-bottom: -2rem;
           }
         }
       `}</style>
