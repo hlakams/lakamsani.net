99ba82597334cc021a0dbdf9f16b913ff2e77a46
Harsha Lakamsani <hlakamsani@gmail.com>
2024-01-04 02:40:42 -0800

    index page

--- theme.config.js
+++ theme.config.js
@@ -2,9 +2,9 @@ const YEAR = new Date().getFullYear();
 
 export default {
   footer: (
-    <small style={{ display: "block", marginTop: "4rem" }}>
-      <abbr title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.">
-        CC BY-NC 4.0</abbr> <time>{YEAR}</time> © Harsha Lakamsani.
+    <small style={{ display: "block", marginTop: "2rem" }}>
+      <abbr title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial-NoDerivs License (CC-BY-NC-ND).">
+        CC-BY-NC-ND</abbr> <time>{YEAR}</time> © Harsha Lakamsani.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
@@ -12,12 +12,12 @@ export default {
         }
         @media screen and (max-width: 480px) {
           article {
-            padding-top: 2rem;
+            padding-top: -2rem;
             padding-bottom: -2rem;
           }
         }
       `}</style>
     </small>
   ),
-  unstable_faviconGlyph: "👋",
+  unstable_faviconGlyph: "🪐",
 };
