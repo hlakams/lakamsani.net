430561c47523561987c31f9be0e661b8c7a8c3ba
Harsha Lakamsani <hlakamsani@gmail.com>
2024-07-01 01:30:04 -0700

    working integration of Dark Visitors agent analytics middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,42 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-    const headers = new Headers();
-    headers.set("Content-Type", "text/plain");
-    
-    return new NextResponse(
-        await getRobotsTXT(),
-        {
-            status: 200,
-            statusText: "OK",
-            headers
-        }
-    );
+    if (req.nextUrl.pathname.match('/((?!api|_next/static|_next/image|favicon.ico).*)')) {
+        await sendAgentAnalytics(req);
+    }
+
+    if (req.nextUrl.pathname.startsWith('/robots.txt')) {
+        const headers = new Headers();
+        headers.set("Content-Type", "text/plain");
+        
+        return new NextResponse(
+            await getRobotsTXT(),
+            {
+                status: 200,
+                statusText: "OK",
+                headers
+            }
+        );
+    }
 }
 
-async function getRobotsTXT() {
+async function sendAgentAnalytics(req: NextRequest): Promise<any> {
+    const url = 'https://api.darkvisitors.com/robots-txts';
+    const agentAnalyticsCall = fetch(url, {
+        method: 'POST',
+        headers: {
+            "Authorization": "Bearer " + process.env.DARK_VISITORS_API_KEY,
+        },
+        body: JSON.stringify({
+            request_path: req.nextUrl.pathname,
+            request_method: req.method,
+            request_headers: req.headers
+        })
+    })
+    return agentAnalyticsCall
+}
+
+async function getRobotsTXT(): Promise<any> {
     const url = 'https://api.darkvisitors.com/robots-txts';
     const robotsTXTCall = fetch(url, {
         method: 'POST',
