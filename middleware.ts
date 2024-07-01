140646b8fa5a6100c478b44df1feda7065ffd6b4
Harsha Lakamsani <hlakamsani@gmail.com>
2024-07-01 04:22:23 -0700

    error handling for fetch requests + cache directives + comments

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,47 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
+const fs = require("fs");
 
-let robotsTXTCall: Promise<any>, agentAnalyticsCall: Promise<any>;
+let robotsTXTCall: Promise<any>, modifiedRobotsTXTData: string;
 
 export async function middleware(req: NextRequest) {
-    if (req.nextUrl.pathname.match('/((?!api|_next/static|_next/image|favicon.ico).*)')) {
-        await sendAgentAnalytics(req);
-    }
+    // optionally remove analytics exposure for site resources
+    /*
+    Pathname matches start with:
+    /api
+    /_next/static
+    /_next/image
+    /favicon.ico
+    */
+    // if (req.nextUrl.pathname.match("^(?!/(api|_next/[static|image]|favicon\.ico))")) {
+    //     await sendAgentAnalytics(req);
+    // }
 
+    // I prefer analytics on all paths
+    try {
+        await sendAgentAnalytics(req);
+    } catch (e) {
+        // if this fails, do not serve data
+        console.log(e);
+        throw new Error(e);
+    }
+        
+    // also generate the robots.txt if not cached
     if (req.nextUrl.pathname.startsWith('/robots.txt')) {
         const headers = new Headers();
         headers.set("Content-Type", "text/plain");
         
+        // take a shot at using the Dark Visitors API
+        try {
+            modifiedRobotsTXTData = await getRobotsTXT()
+        } catch (e) {
+            // if that fails, we can use a static robots.txt
+            console.log(e);
+            modifiedRobotsTXTData = readFile("public/robots.txt")
+        }
+
+        // serve (hopefully cached) robots.txt
         return new NextResponse(
-            await getRobotsTXT(),
+            modifiedRobotsTXTData,
             {
                 status: 200,
                 statusText: "OK",
@@ -22,9 +51,11 @@ export async function middleware(req: NextRequest) {
     }
 }
 
+// note: do not cache fetch since it is unique for each path hit
 async function sendAgentAnalytics(req: NextRequest): Promise<any> {
     const url = 'https://api.darkvisitors.com/robots-txts';
-    agentAnalyticsCall = fetch(url, {
+    const agentAnalyticsCall = fetch(url, {
+        cache: 'no-store',
         method: 'POST',
         headers: {
             "Authorization": "Bearer " + process.env.DARK_VISITORS_API_KEY,
@@ -38,6 +69,7 @@ async function sendAgentAnalytics(req: NextRequest): Promise<any> {
     return agentAnalyticsCall
 }
 
+// note: this response needs to be cached!
 async function getRobotsTXT(): Promise<any> {
     const url = 'https://api.darkvisitors.com/robots-txts';
     robotsTXTCall = fetch(url, {
@@ -61,8 +93,9 @@ async function getRobotsTXT(): Promise<any> {
     });
 
     return robotsTXTCall;
-} 
-
-export const config = {
-    matcher: '/robots.txt',
 }
+
+// helper function to read static files for serving failback requests
+const readFile = function (file: string) : string {
+    return fs.readFileSync(file).toString();
+};
